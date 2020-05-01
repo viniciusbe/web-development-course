21ac21bc3a09639543b4b5443342676d2c194451
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2020-04-30 21:48:12 -0300

    commit on master

--- 3.js
+++
@@ -1,6 +0,0 @@
-const exponenciacao = (base, expoente) => Math.pow(base, expoente)
-
-const base = 5
-const expoente = 3
-
-console.log(`${base} elevado á ${expoente} é ${exponenciacao(base, expoente)}`)
