5543d2ea80d618021b3de02e174f87f218cd2e64
Vinicius <vinibsp10@gmail.com>
2020-05-11 22:29:51 -0300

    concat

---
+++ aulas/array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Ana', 'Paula']
+const filhos = ['Felipe', 'Thiago']
+const todos = filhas.concat(filhos)
+
+console.log(todos)
+
+console.log(['a', 'b'].concat([1, 2], [3, 4], 5, [[6, 7]]))
