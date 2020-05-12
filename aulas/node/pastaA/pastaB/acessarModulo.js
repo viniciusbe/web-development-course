611ca3b129f5dc36d7faa0c28245514a65d1c89f
viniciusbe <vinicius.bernardesds@gmail.com>
2020-05-12 18:48:05 -0300

    modulo

---
+++ aulas/node/pastaA/pastaB/acessarModulo.js
@@ -0,0 +1,14 @@
+const moduloA = require('../../moduloA')
+console.log(moduloA.ola)
+
+const c = require('./pastaC')
+console.log(c.ola2)
+
+// const saudacao = require('saudacao')
+// console.log(saudacao.ola)
+
+// const http = require('http')
+// http.createServer((req,res) => {
+//      res.write('Bom dia!')
+//      res.end()
+//  }).listen(8080)
