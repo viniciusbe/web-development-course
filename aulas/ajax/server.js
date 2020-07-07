a1281aeef4cb9defde8b8a1a7b1c27e71fa5707a
viniciusbe <vinicius.bernardesds@gmail.com>
2020-07-07 11:31:16 -0300

    ajax

--- aulas/ajax/server.js
+++ aulas/ajax/server.js
@@ -9,10 +9,10 @@ app.use(bodyParser.json())
 const multer = require('multer')
 
 const storage = multer.diskStorage({
-    destination: function(req, file, callback) {
-        callback(null,'./upload')
+    destination: function (req, file, callback) {
+        callback(null, './upload')
     },
-    filename: function(req,file,callback) {
+    filename: function (req, file, callback) {
         callback(null, `${Date.now()}_${file.originalname}`)
     }
 
@@ -21,12 +21,29 @@ const storage = multer.diskStorage({
 const upload = multer({ storage }).single('arquivo')
 
 app.post('/upload', (req, res) => {
-    upload(req,res, err => {
-        if(err) {
+    upload(req, res, err => {
+        if (err) {
             return res.end('Ocorreu um erro.')
         }
 
         res.end('Concluído com sucesso.')
+    })
+})
+
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
+app.get('/parOuImpar',(req, res) => {
+    // req.body
+    // req.query
+    // req.params
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
     })
 })
 
