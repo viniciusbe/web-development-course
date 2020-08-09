08bf372a4e529e9e00203c7cb0ce71cd4b6959f7
viniciusbe <vinicius.bernardesds@gmail.com>
2020-08-09 00:50:13 -0300

    gulp

---
+++ aulas/gulp/spa/gulpTasks/app.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp')
+
+function appHtml(cb) {
+    return cb()
+}
+
+function appCSS(cb) {
+    return cb()
+}
+
+function appJS(cb) {
+    return cb()
+}
+
+function appIMG(cb) {
+    return cb()
+}
+
+module.exports = {
+    appHtml,
+    appCSS,
+    appJS,
+    appIMG
+}
