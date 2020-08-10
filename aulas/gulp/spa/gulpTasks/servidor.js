f3796050b6392b634bfe739453e2d6d05df000cd
viniciusbe <vinicius.bernardesds@gmail.com>
2020-08-10 02:04:08 -0300

    spa-gulp

--- aulas/gulp/spa/gulpTasks/servidor.js
+++ aulas/gulp/spa/gulpTasks/servidor.js
@@ -1,13 +1,22 @@
 const gulp = require('gulp')
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
 
-function monitorarArquivos(cb) {
-
-    return cb()
+function monitorarArquivos() {
+    watch('src/**/*.html', () => gulp.series('appHtml')())
+    watch('src/**/*.scss', () => gulp.series('appCSS')())
+    watch('src/**/*.js', () => gulp.series('appJS')())
+    watch('src/assets/imgs/**/*.*', () => gulp.series('appIMG')())
+    return
 }
 
-function servidor(cb) {
-
-    return cb()
+function servidor() {
+    return gulp.src('build')
+        .pipe(webserver({
+            port: 8080,
+            open: true,
+            livereload: true
+        }))
 }
 
 module.exports = {
