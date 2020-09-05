5beab789ab2f495ed002dd2881c4aa4afaced8ce
viniciusbe <vinicius.bernardesds@gmail.com>
2020-09-05 00:42:22 -0300

    react

--- aulas/react/exercicios/src/index.js
+++ aulas/react/exercicios/src/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // o nome dos  componentes deve começar com letra maiuscula
 import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
     <div>
-        <Pai nome="Paulo" sobrenome="Silva"></Pai>
+        <Pai nome="Paulo" sobrenome="Silva">
+            <Filho nome="Pedro"></Filho>
+            <Filho nome="Paulo" sobrenome="Silva"></Filho>
+            <Filho nome="Carla" sobrenome="Silva"></Filho>
+        </Pai>
     </div>
     , document.getElementById('root'))
