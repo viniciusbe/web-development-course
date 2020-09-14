59243e0a53ad7e62204b2a214de597b026c85c1f
viniciusbe <vinicius.bernardesds@gmail.com>
2020-09-13 22:58:25 -0300

    projeto-crud

---
+++ aulas/react/crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Segundo Projeto do capítulo de React.">
+        <div className="display-4">Bem vindo!</div>
+        <hr />
+        <p className="mb-0"> Cadastro desenvolvido em React!</p>
+    </Main>
