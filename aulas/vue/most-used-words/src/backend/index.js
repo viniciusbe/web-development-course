3b61122060bdfe250973f22302aac9cb181cbc69
viniciusbe <vinicius.bernardesds@gmail.com>
2020-09-22 23:19:52 -0300

    vue-muw

--- aulas/vue/most-used-words/src/backend/index.js
+++ aulas/vue/most-used-words/src/backend/index.js
@@ -2,21 +2,15 @@ const { ipcMain } = require('electron')
 
 const pathsToRows = require('./pathsToRows')
 const prepareData = require('./prepareData')
+const groupWords = require('./groupWords')
 
 ipcMain.on('process-subtitles', (event, paths) => {
 
     pathsToRows(paths)
         .then(rows => prepareData(rows))
-        .then(words => console.log(words))
-        .then(() => {
-            event.reply('process-subtitles', [
-                { name: "i", amount: 1250 },
-                { name: "you", amount: 900 },
-                { name: "he", amount: 850 },
-                { name: "she", amount: 850 },
-                { name: "our", amount: 232 },
-                { name: "house", amount: 22 },
-            ])
+        .then(words => groupWords(words))
+        .then(groupedWords => {
+            event.reply('process-subtitles', groupedWords)
         })
 
 })
