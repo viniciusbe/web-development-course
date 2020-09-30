206a67c3ca221a9480ebc2c9dcc39ba61536697a
viniciusbe <vinicius.bernardesds@gmail.com>
2020-09-29 23:11:17 -0300

    express

--- aulas/exercicios-express/index.js
+++ aulas/exercicios-express/index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 
+app.use('/opa', (req, res) => {
+    res.json([
+        { id: 7, name: 'Ana', position: 1 },
+        { id: 34, name: 'Bia', position: 2 },
+        { id: 72, name: 'Carlos', position: 3 }
+    ])
+
+    // res.json({
+    //     name:'iPad 64gb',
+    //     price:1899.00,
+    //     discount:0.12
+    // })
+
+    // res.send('<h1>Estou bem!</h1>')
+})
 app.listen(3000, () => {
     console.log("Backend rodando...")
 })
