92536e6b8f1b7fee8e5ed1eb4211778bada34bba
viniciusbe <vinicius.bernardesds@gmail.com>
2020-09-30 12:07:42 -0300

    express

---
+++ aulas/exercicios-express/api/produto.js
@@ -0,0 +1,16 @@
+const { text } = require("body-parser");
+
+module.exports = (app, texto) => {
+    function salvar(req, res) {
+        res.send('Produto > salvar > ' + texto)
+    }
+
+    function obter(req, res) {
+        res.send('Produto > obter > ' + texto)
+    }
+
+    app.post('/produto', salvar)
+    app.get('/produto', obter)
+    
+    return { salvar, obter }
+}
--- aulas/exercicios-express/index.js
+++ aulas/exercicios-express/index.js
@@ -1,7 +1,52 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-app.use('/opa', (req, res) => {
+const saudacao = require('./saudacaoMid')
+const usuarioApi = require('./api/usuario')
+const produtoApi = require('./api/produto')
+
+produtoApi(app,'com param!')
+
+
+app.post('/usuario', usuarioApi.salvar)
+app.get('/usuario', usuarioApi.obter)
+
+app.use(express.text())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(saudacao('Guilherme'))
+
+
+app.get('/clientes/relatorio', (req, res) => {
+    res.send(`Cliente relatorio: completo ${req.query.completo} ano = ${req.query.ano}`)
+})
+
+app.post('/corpo', (req, res) => {
+    // let corpo = ''
+    // req.on('data', function (parte) {
+    //     corpo += parte
+    // })
+
+    // req.on('end', function () {
+    //     res.send(corpo)
+    // })
+    res.send(req.body)
+})
+
+
+app.get('/clientes/:id', (req, res) => {
+    res.send(`Cliente ${req.params.id} selecionado!`)
+})
+
+
+app.use('/opa', (req, res, next) => {
+    console.log('Serase serei chamado?')
+    next()
+})
+
+app.get('/opa', (req, res) => {
     res.json([
         { id: 7, name: 'Ana', position: 1 },
         { id: 34, name: 'Bia', position: 2 },
@@ -16,6 +61,10 @@ app.use('/opa', (req, res) => {
 
     // res.send('<h1>Estou bem!</h1>')
 })
+
+
+
+
 app.listen(3000, () => {
     console.log("Backend rodando...")
 })
