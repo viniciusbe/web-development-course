f1683723c6836b00409a36044ec2ef8338102c46
viniciusbe <vinicius.bernardesds@gmail.com>
2020-10-07 11:55:51 -0300

    promises

---
+++ aulas/esnext/desafio.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const { resolve } = require('path')
+const path = require('path')
+
+
+function lerArquivo(caminho) {
+    return new Promise(resolve => {
+        fs.readFile(caminho, function (_, conteudo) {
+            resolve(conteudo.toString())
+        })
+        console.log('Depois de ler')
+    })
+}
+
+const caminho = path.join(__dirname, 'dados.txt')
+
+lerArquivo(caminho).then(console.log)
